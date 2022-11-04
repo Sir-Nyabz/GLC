f4e3a9dd52c88b2b3944c177cf3690d409f41288
Isaac Nyaabila <nyaabilai@gmail.com>
2022-11-04 03:47:32 +0000

    alter

--- src/app/components/member-biodata/member-biodata.component.ts
+++ src/app/components/member-biodata/member-biodata.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder, AbstractControl, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { concatMap, tap } from 'rxjs';
 import { MemberService } from 'src/app/shared/member.service';
 import { UserService } from '../../shared/user.service';
@@ -28,7 +29,7 @@ export class MemberBiodataComponent implements OnInit {
   branch_uuid: any;
 
 
-  constructor(private userService: UserService, private memberService: MemberService,private formBuilder:FormBuilder) { 
+  constructor(private userService: UserService, private memberService: MemberService,private formBuilder:FormBuilder,private router:Router) { 
     this.biodataGroup=this.formBuilder.group({
       first_name:['',[Validators.required]],
       email1:['',[Validators.required,Validators.email]],
@@ -80,7 +81,54 @@ export class MemberBiodataComponent implements OnInit {
 
   addBiodata(){
     this.submitted=true;
-    console.log(this.biodataGroup.value)
+
+    const email = this.biodataGroup.value.email;
+    const first_name=this.biodataGroup.value.first_name;
+    const last_name=this.biodataGroup.value.last_name;
+    const other_name=this.biodataGroup.value.other_name;
+    const gender=this.biodataGroup.value.gender;
+    const date_of_birth=this.biodataGroup.value.date_of_birth;
+    const place_of_birth=this.biodataGroup.value.place_of_birth;
+    const home_town=this.biodataGroup.value.home_town;
+    const region_uuid=this.biodataGroup.value.region_uuid;
+    const postal_address=this.biodataGroup.value.postal_address;
+    const residential_address=this.biodataGroup.value.residential_address;
+    const occupation=this.biodataGroup.value.occupation;
+    const membership_number=this.biodataGroup.value.membership_number;
+    const number_of_children=this.biodataGroup.value.number_of_children;
+    const marital_status=this.biodataGroup.value.number_of_children;
+    const branch_uuid=this.biodataGroup.value.branch_uuid;
+
+
+    this.memberService.addAsorebaProfile(
+      email,
+      first_name,
+    last_name,
+    other_name,
+    gender,
+    date_of_birth,
+    place_of_birth,
+    home_town,
+    region_uuid,
+    postal_address,
+    residential_address,
+    occupation,
+    membership_number,
+    number_of_children,
+    marital_status,
+    branch_uuid).subscribe((res:any)=>{
+      if(res){
+        this.router.navigate(['/forgot']);
+      //form.reset();
+      }else{
+        alert("Email does not exist");
+        //form.reset();
+      }
+      
+    },
+    err=>{
+      console.log(err);
+    })
   }
 
   addContact(){
